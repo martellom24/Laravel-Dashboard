357f827ea42cd095df8bc7447f8a6146510983b5
martellom24 <martellom24@gmail.com>
2019-03-13 14:30:07 -0400

    added contact page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="starter-template">
+            <h1>Contact: <span class="d-flex display-3">Mike Martello</span></h1>
+        </div>
+    </div>
+@endsection
