e79f73b18594af9d35164aeed02a07e3927b235e
martellom24 <martellom24@gmail.com>
2019-03-13 14:27:45 -0400

    about.blade.php additions

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="container-fluid">
         <div class="starter-template">
-            <h1>Bootstrap starter template</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+            <h1>Dashboard create by: <span class="d-flex display-3">Mike Martello</span></h1>
+            <p class="lead">This is a custom dashboard that will pull in data from company Net and Revenue from multiple data points.</p>
+            <p class="lead">Alternatively, view current products and monitoring of store assests will be a secondary feature.</p>
         </div>
     </div>
 @endsection
