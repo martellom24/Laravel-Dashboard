09fd8b412c06194a86fd71aa8652fa4da11f3329
martellom24 <martellom24@gmail.com>
2019-03-25 19:16:32 -0400

    css active class for nav

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -3,8 +3,8 @@
         <div class="inner">
             <h3 class="masthead-brand">Laravel Web App</h3>
             <nav class="nav nav-masthead justify-content-center">
-                <a class="nav-link" href="{{route('home')}}">Home</a>
-                <a class="nav-link" href="{{route('contact')}}">Contact</a>
+                <a class="nav-link {{(request()->is('/')) ? 'active' : '' }}" href="{{route('home')}}">Home</a>
+                <a class="nav-link {{ (request()->is('contact')) ? 'active' : '' }}" href="{{route('contact')}}">Contact</a>
             </nav>
         </div>
     </header>
