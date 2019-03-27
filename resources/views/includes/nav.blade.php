432c2785407a08a1cb93d64b3f0f2a6c3583e6fb
martellom24 <martellom24@gmail.com>
2019-03-27 10:54:12 -0400

    nav-blade.php

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,6 +1,6 @@
 <header class="masthead mb-auto text-center">
     <div class="inner">
-        <h3 class="masthead-brand"><i class="fa fa-universal-access" aria-hidden="true"></i>&nbsp;Web App </h3>
+        <h3 class="masthead-brand"><i class="fa fa-universal-access" aria-hidden="true"></i>&nbsp;Web App</h3>
         <nav class="nav nav-masthead justify-content-center">
             <a class="nav-link {{ (request()->is('/')) ? 'active' : '' }}" href="{{route('home')}}">Home</a>
             <a class="nav-link {{(request()->is('about')) ? 'active' : ''}}" href="{{route('about')}}">About</a>
